6beb2b74c88ceb66518cfbdc9468f48c723c9cb2
Maialen45 <maialenes@gmail.com>
2025-05-15 21:26:09 +0200

    new: primer commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Juego de Adivinanzas
+
+Este es un pequeño y divertido juego web donde el usuario intenta adivinar un número secreto entre 1 y 100. El juego da pistas si el número ingresado es mayor o menor que el número misterioso, y lleva un contador de intentos. ¡Ideal para practicar JavaScript interactivo!
+
+![Captura de pantalla de la aplicación](./assets/images/adivinanza-numeros.png)
+
+## Tecnologías utilizadas
+
+-   HTML5
+-   CSS3
+-   JavaScript
+
+## Cómo funciona
+
+1. El juego genera un número aleatorio entre 1 y 100.
+2. El usuario introduce un número en el campo de entrada.
+3. Al hacer clic en "Comprobar":
+    - Si el número es correcto, muestra un mensaje de éxito.
+    - Si es menor o mayor, da una pista visual.
+4. Se muestra el número de intentos realizados.
+5. El botón "Reiniciar" genera un nuevo número misterioso y reinicia el contador.
+
+## Cómo utilizar
+
+1. Clona este repositorio:
+    ```bash
+    git clone https://github.com/Maialen45/juego-adivinanzas.git
+    ```
+2. Abre `index.html` con cualquier navegador para ver el sitio web.
---
+++ js/script.js
@@ -0,0 +1,54 @@
+function generadorNumero(min, max) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+var numeroMisterioso = generadorNumero(0, 101);
+
+function comprobar() {
+	let numero = parseInt(document.getElementById("numero").value);
+	let inputNum = document.getElementById("numero");
+	let resultadoMenor = document.getElementById("menor");
+	let resultadoMayor = document.getElementById("mayor");
+	if (inputNum.value > 100) {
+		alert("Introduce un número entre 1 y 100");
+		inputNum.value = "";
+	} else {
+		if (numero == numeroMisterioso) {
+			inputNum.value = "";
+			window.alert("Enhorabuena, has acertado el número");
+		} else if (numero > numeroMisterioso) {
+			resultadoMenor.textContent = `El número es menor que ${numero}`;
+			inputNum.value = "";
+		} else if (numero < numeroMisterioso) {
+			resultadoMayor.textContent = `El número es mayor que ${numero}`;
+			inputNum.value = "";
+		} else {
+			window.alert("Introduce un número");
+		}
+	}
+	cont();
+}
+
+var contador = 0;
+
+function reiniciar() {
+	numeroMisterioso = generadorNumero(0, 101);
+	let resultadoMenor = document.getElementById("menor");
+	let resultadoMayor = document.getElementById("mayor");
+	resultadoMenor.textContent = "";
+	resultadoMayor.textContent = "";
+	contador = 0;
+	let intentos = document.getElementById("intentos");
+	intentos.innerHTML = `Has realizado ${contador} intentos`;
+}
+
+function cont() {
+	contador += 1;
+	let intentos = document.getElementById("intentos");
+	intentos.innerHTML = `Has realizado ${contador} intentos`;
+}
+
+document.getElementById("comprobador").addEventListener("click", comprobar);
+document.getElementById("reiniciador").addEventListener("click", reiniciar);
